0961853bf5f86583621f3bea5531a3ec661544e6
jay <jaypifiverr@gmail.com>
2022-12-21 08:01:43 -0500

    commit tree fa61fbd475c20f5492f5be16c8bb4d74150abce1

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,11 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+
+require 'pry'
+
+def parrot(speak = "Squawk!")
+    puts speak
+    return speak
+end
+
+binding.pry
